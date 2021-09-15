785b1b76404b067d2191712d2230b54072f368c4
l3043y <boreyc2013@gmail.com>
2021-09-15 02:46:32 -0400

    Add iot-server.php

--- codebase/public/iot-server.php
+++ codebase/public/iot-server.php
@@ -1,3 +1,31 @@
 <?
-echo "Welecome to iot-server"
+$servername = "172.20.0.2";
+$username = "root";
+$password = "superSecr3t";
+$method = $_SERVER['REQUEST_METHOD'];
+echo "$method::Welcome to iot-server\n";
+
+// Create connection
+echo "DATABASE_URL=mysql://$username:$password@$servername:PORT_NUMBER/\r\n";
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully!\r\n";
+
+// get posted data
+if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+    $data = json_decode(file_get_contents("php://input"));
+
+    $sql = "INSERT INTO admin_iot.eMeter (identifier, value) VALUES ('$data->identifier', '$data->value')";
+    if(mysqli_query($conn, $sql)){
+        echo "Records added successfully!!\r\n";
+    } else{
+        echo "ERROR: Could not able to execute $sql.\r\n" . mysqli_error($conn);
+    }
+    echo json_encode($data);
+}
+
 ?>
